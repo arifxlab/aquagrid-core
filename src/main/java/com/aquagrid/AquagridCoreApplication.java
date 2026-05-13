bb8c2673051ac6b35fd3a2586233198116dda915
Arif Khan <arifxlab@gmail.com>
2026-05-13 12:00:31 +0500

    Initial AquaGrid backend setup

---
+++ src/main/java/com/aquagrid/AquagridCoreApplication.java
@@ -0,0 +1,13 @@
+package com.aquagrid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquagridCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AquagridCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aquagrid/AquagridCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aquagrid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AquagridCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
